3d9e466c9bc271ae42270dd68eec68a59cf26578
Danil <47692381+SelDanilEv@users.noreply.github.com>
2024-12-31 16:24:14 +0100

    Adding pwa

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/styles.scss";
+import './registerServiceWorker'
 
 createApp(App).mount("#app");
--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: 'Binary Options Trading',
+    themeColor: '#1e88e5',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      background_color: '#ffffff'
+    },
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+    }
+  }
 });
